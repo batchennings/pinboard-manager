74f15e12a551734b927976fafe3d9f5a8546ef3a
patjennings <patjennings@macbook-pro-de-patjennings.home>
2023-06-30 22:11:14 +0200

    css update

--- images/static/js/scripts.js
+++ images/static/js/scripts.js
@@ -3,4 +3,29 @@
 
 window.onload = function(e){
     console.log("JS for pinboard manager is ready");
+    ThemeManagement();
+    LabelsManagement();
+}
+function ThemeManagement(){
+    const ts = document.getElementById('theme-selector');
+    ts.addEventListener('click', function(){
+        if (ts.checked) {
+            document.body.classList.add('dark');
+        } else {
+            document.body.classList.remove('dark');
+        }
+    });
+}
+
+function LabelsManagement(){
+    const ls = document.getElementById('label-selector');
+    const listLabels = document.querySelectorAll('.image-label');
+
+    ls.addEventListener('click', function(){
+        if (ls.checked) {
+            listLabels.forEach(l => l.classList.remove('hidden'));
+        } else {
+            listLabels.forEach(l => l.classList.add('hidden'));
+        }
+    });
 }
--- images/static/tailwind.config.js
+++ images/static/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
         './**/*.html',
         '../**/*.html'
     ],
+    darkMode: 'class',
     theme: {
         screens: {
             'phone': '480px',
@@ -11,40 +12,6 @@ module.exports = {
             'laptop': '1024px',
             'desktop': '1440px',
         },
-        extend: {
-            backgroundImage: {
-                'header-image': 'url("assets/images/header.png")',
-                'site-texture': ''
-            }
-        },
-        colors: {
-            transparent: 'transparent',
-            'white': '#ffffff',
-            blue: {
-                400: '#367CCF',
-                600: '#2B5485',
-                750: '#1D334E',
-                800: '#0F2743'
-            },
-            orange: {
-                200: '#f8f8ee',
-                600: '#FABA2F'
-            },
-            green: {
-                800: '#23844D',
-                600: '#38C674'
-            },
-            gray: {
-                100: '#EBEAF1',
-                200: '#DCDAE7',
-                300: '#B3B0C1',
-                400: '#706D81',
-                500: '#444251',
-                700: '#26252E',
-                900: '#121216',
-            }
-        }
-    },
-    plugins: [],
+    }
 }
 
