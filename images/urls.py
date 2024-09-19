ed54dadbbf1f83808daf40862ceeffd0072765f2
Thomas Guesnon <>
2024-09-19 20:56:18 +0200

    changes urls

--- images/urls.py
+++ images/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('images/', views.images, name='images'),
-    path('images/tags', views.tags, name='tags'),
-    path('images/image/<int:id>', views.image, name='image'),
-    path('images/image/<int:id>/tags_update', views.tags_update, name='tags_update'),
-    path('images/image/<int:id>/delete', views.image_delete, name='image_delete'),
+    path('', views.images, name='images'),
+    path('tags', views.tags, name='tags'),
+    path('image/<int:id>', views.image, name='image'),
+    path('image/<int:id>/tags_update', views.tags_update, name='tags_update'),
+    path('image/<int:id>/delete', views.image_delete, name='image_delete'),
 ]
